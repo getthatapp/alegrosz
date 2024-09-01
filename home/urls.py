2b057eadeff769daf4a45fc7ba71f9480c496566
Grzegorz Turek <g.turek@yahoo.com>
2024-09-01 17:09:33 +0200

    configuration for django templates

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'home'
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<h1>Alegrosz</h1>')
+    return render(request, 'home/home.html')
 
-def contact(request):
-    return HttpResponse('<h1>Contact</h1>')
+
