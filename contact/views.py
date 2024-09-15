b49756d23764dc308fdd6758dfdfb2c0b6c9308b
Grzegorz Turek <g.turek@yahoo.com>
2024-09-15 18:42:06 +0200

    forms, crips

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,14 @@
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from . import forms
+from . import models
 
-from django.shortcuts import render
 
+class ContactView(FormView):
+    template_name = 'contact/contact.html'
+    form_class = forms.ContactForm
+    success_url = reverse_lazy('home:home')
 
-def contact(request):
-    return render(request, 'contact/contact.html')
+    def form_valid(self, form):
+        models.Contact.objects.create(**form.cleaned_data)
+        return super().form_valid(form)
